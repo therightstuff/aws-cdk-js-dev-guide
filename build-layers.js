6da27245c9f049e88609614cd0a33b1438e7800c
therightstuff <adam@industrialcuriosity.com>
2020-08-08 12:46:37 +0200

    removed external dependency for layer build script

--- build-layers.js
+++ build-layers.js
@@ -4,14 +4,33 @@
 // the layers/src folders, compresses the results and moves
 // them into the layers/build folder
 
+const archiver = require('archiver');
 const fs = require("fs");
 const fse = require("fs-extra");
 const process = require("child_process");
+
+function zipDirectory(source, archivedName, out) {
+    const archive = archiver('zip', { zlib: { level: 9 }});
+    const stream = fs.createWriteStream(out);
+
+    return new Promise((resolve, reject) => {
+        archive
+            .directory(source, archivedName)
+            .on('error', err => reject(err))
+            .pipe(stream)
+        ;
+
+        stream.on('close', () => resolve());
+        archive.finalize();
+    });
+}
 
 console.log('building layers...')
 
 // ensure layers directory created
 fs.mkdirSync('layers/src', {recursive: true});
+fs.rmdirSync('layers/build', { recursive: true })
+
 // get layers' src directories
 let srcdirs = fs.readdirSync('layers/src', { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
@@ -24,9 +43,7 @@ for (let i in srcdirs) {
     let layerSrcPath = `layers/src/${layer}`
     let layerBuildPath = `layers/build/${layer}/nodejs`
 
-    console.log(`removing existing build folder...`);
-    fse.removeSync(layerBuildPath);
-    fs.mkdirSync(layerBuildPath, {recursive: true});
+    fs.mkdirSync(layerBuildPath, { recursive: true });
 
     // copy everything except the package-lock file and node_modules
     let srcContents = fs.readdirSync(layerSrcPath, { withFileTypes: true })
@@ -52,7 +69,15 @@ for (let i in srcdirs) {
     fs.unlinkSync(`${layerBuildPath}/package-lock.json`);
 
     console.log("creating layer archive...");
-    process.execSync(`zip -r ${layer}.zip .`, { cwd: `layers/build/${layer}` });
+
+    zipDirectory(`layers/build/${layer}/nodejs`, 'nodejs', `layers/build/${layer}/${layer}.zip`)
+    .then(() => {
+        console.log(`${layer} folder compressed successfully`);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
 }
 
 console.log('layer builds completed.')

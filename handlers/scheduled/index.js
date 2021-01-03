ab595fe5a695bd041448f95d88ba1a476c243171
therightstuff <adam@industrialcuriosity.com>
2021-01-03 11:48:23 +0200

    updated scheduled lambda with example warmup handling

--- handlers/scheduled/index.js
+++ handlers/scheduled/index.js
@@ -1,3 +1,24 @@
 exports.handler = async (event) => {
     console.log("scheduled lambda function called.")
+
+    // warmup calls do not have event.pathParameters defined
+    if (!event.pathParameters) {
+        console.log('warmup call detected, ending');
+        return;
+    }
+
+    return new Promise((resolve, reject) => {
+        resolve({
+            "isBase64Encoded": false,
+            "statusCode": 200,
+            "headers": {
+                'Access-Control-Allow-Origin': process.env.CORS_ORIGIN,
+                'Access-Control-Allow-Credentials': true,
+            },
+            "body": JSON.stringify({
+                "success": true,
+                "message": "This is a the response to an unscheduled HTTP request"
+            })
+        });
+    });
 }

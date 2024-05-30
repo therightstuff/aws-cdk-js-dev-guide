ae7cfb1d98c337339626493822413a8a7b8ab2ae
therightstuff <adam@industrialcuriosity.com>
2024-05-30 21:38:01 +0300

    bump deps

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    languageOptions: {
+        parserOptions: {
+            ecmaVersion: 2020,
+            sourceType: "module"
+        },
+        globals: {
+            es6: true
+        }
+    },
+};

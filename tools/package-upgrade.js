f6d1ce22092a60984bb135c336fc3293df51d715
therightstuff <therightstuff@industrialcuriosity.com>
2025-12-11 09:36:53 +0200

    bump deps

--- tools/package-upgrade.js
+++ tools/package-upgrade.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const childProcess = require("child_process");
 
 console.log(`updating cdk project packages`);
-childProcess.execSync("npx npm-check-updates -u", {
+childProcess.execSync("npx --yes npm-check-updates -u", {
     stdio: "inherit",
     stderr: "inherit",
 });
@@ -18,7 +18,7 @@ for (let i in srcdirs) {
     console.log(`\nupdating packages for layer ${layer}...`);
 
     let layerSrcPath = `layers/src/${layer}`;
-    childProcess.execSync("npx npm-check-updates -u", {
+    childProcess.execSync("npx --yes npm-check-updates -u", {
         cwd: layerSrcPath,
         stdio: "inherit",
         stderr: "inherit",
